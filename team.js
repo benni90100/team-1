9d8b7a31adb5c12d51fe382964cd1a057e7f53ec
benni90100 <micelibenedetto800@gmail.com>
2023-10-27 11:58:26 +0200

    creato metodo costruttore

---
+++ team.js
@@ -0,0 +1,12 @@
+let persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
+    this.name=name,
+    this.surname=surname,
+    thisage=age,
+    this.city=city,
+    this.hobby=hobby,
+    this.favFood=favFood,
+    this.favVideogame=favVideogame,
+    this.favFilm=favFilm
+    this.favFood=favBook,
+    this.petName=petName,
+}
