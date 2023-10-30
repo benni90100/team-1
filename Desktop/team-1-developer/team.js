1fc262cfae8ee81a10e275a9cbd7cc4f9f42202a
ChiaRa <148362799+chiaRapisar@users.noreply.github.com>
2023-10-30 16:58:47 +0100

    ChiaRa

---
+++ Desktop/team-1-developer/team.js
@@ -0,0 +1,14 @@
+function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
+    this.name=name;
+    this.surname=surname;
+    this.age=age;
+    this.city=city;
+    this.hobby=hobby;
+    this.favFood=favFood;
+    this.favVideogame=favVideogame;
+    this.favFilm=favFilm;
+    this.favFood=favBook;
+    this.petName=petName;
+}
+let ChiaraRa = new persona ("Chiara", "Rapisardi", 27, "Palermo", "Natura", "Mandorle", "", "Interstate 60", "L'alchimista", "Ernestina");
+console.log(ChiaraRa);
