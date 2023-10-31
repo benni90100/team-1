b42b38895c66cbac1706344946f795aa83efde0a
Enrico Caliandro <enrico.caliandro.94@gmail.com>
2023-10-31 16:25:05 +0100

    aggiunta persona enrico

--- team.js
+++ team.js
@@ -1,13 +1,14 @@
-function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-    this.city=city;
-    this.hobby=hobby;
-    this.favFood=favFood;
-    this.favVideogame=favVideogame;
-    this.favFilm=favFilm;
-    this.favFood=favBook;
-    this.petName=petName;
+function persona(name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.city = city;
+    this.hobby = hobby;
+    this.favFood = favFood;
+    this.favVideogame = favVideogame;
+    this.favFilm = favFilm;
+    this.favFood = favBook;
+    this.petName = petName;
 }
 
+let enrico = new persona("Enrico", "Caliandro", 28, "Grottaglie", "drawing", "pasta", "Crash Bandicoot", "into the Spiderverse", "Il manuale del guerriero della luce", "Miele");
