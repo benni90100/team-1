f938da09f79df1afe0c7f9ae6d91188155df6fb0
benni90100 <micelibenedetto800@gmail.com>
2023-11-02 09:46:00 +0100

    aggiunto oggetto benni

--- team.js
+++ team.js
@@ -1,16 +1,14 @@
-function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-    this.city=city;
-    this.hobby=hobby;
-    this.favFood=favFood;
-    this.favVideogame=favVideogame;
-    this.favFilm=favFilm;
-    this.favFood=favBook;
-    this.petName=petName;
-}
-
-let Benny = new persona("benni", "Miceli", 33, "Palermo", "musica", "lasagne", "Rome of war", "Inception",
- "Tropico del capricorno", "jump");
- console.log(Benny);
+let Persona = [
+    Benny = {
+        name: "Benni"
+    surname: "Miceli"
+    age: 33
+    city: "Palermo"
+    hobby: "muusica"
+    favoriteFood: "lasagne"
+    favoriteVideoGame: "CoD"
+    favoriteFilm: "inception"
+    favoriteBook: "tropicoo del cancro"
+    petName: "Jump"
+    }
+]
