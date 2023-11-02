5b9c71671d622b5e3ec395f1631d5d81e0531b99
benni90100 <micelibenedetto800@gmail.com>
2023-11-02 10:25:55 +0100

    add array

--- team.js
+++ team.js
@@ -1,14 +1,14 @@
 let Persona = [
     Benny = {
-        name: "Benni"
-    surname: "Miceli"
-    age: 33
-    city: "Palermo"
-    hobby: "musica"
-    favoriteFood: "lasagne"
-    favoriteVideoGame: "CoD"
-    favoriteFilm: "inception"
-    favoriteBook: "tropicoo del cancro"
-    petName: "Jump"
+    name: "Benni",
+    surname: "Miceli",
+    age: 33,
+    city: "Palermo",
+    hobby: "musica",
+    favoriteFood: "lasagne",
+    favoriteVideoGame: "CoD",
+    favoriteFilm: "inception",
+    favoriteBook: "tropicoo del cancro",
+    petName: "Jump",
     }
 ]
