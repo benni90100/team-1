2aca5e6845bc9566937a81d42fa7eb88ec454fee
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-11-02 10:08:40 +0100

    modifica ChiaRa

--- team.js
+++ team.js
@@ -1,13 +1,14 @@
 
-let Persona =[ 
-    ChiaRa={
-name: "Chiara",
-    surname: "Rapisardi",
-    age: 27,
-    city: "Palermo",
-    hobby:"Natura",
-    favoriteFood:"Mandorle",
-    favoriteVideoGame:"",
-    favoriteFilm:"Interstate 60",
-    favoriteBook:"L'alchimista",
-    petName: "Ernestina",}]
+let Persona = [
+    ChiaRa = {
+        name: "Chiara",
+        surname: "Rapisardi",
+        age: 27,
+        city: "Palermo",
+        hobby: "Natura",
+        favoriteFood: "Mandorle",
+        favoriteVideoGame: "Soulblade(ps1)",
+        favoriteFilm: "Interstate 60",
+        favoriteBook: "L'alchimista",
+        petName: "Ernestina",
+    }]
