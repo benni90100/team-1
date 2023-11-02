1e23854cd0120b11477acfd62161b3c13a0ff4c8
Alessandro <alessandrofrancescocastelli@outlook.it>
2023-11-02 09:45:52 +0100

    aggiornato Alesandro

--- team.js
+++ team.js
@@ -1,15 +1,2 @@
-function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-    this.city=city;
-    this.hobby=hobby;
-    this.favFood=favFood;
-    this.favVideogame=favVideogame;
-    this.favFilm=favFilm;
-    this.favFood=favBook;
-    this.petName=petName;
-}
-
-let alessandro = new persona ("Alessandro","Castelli",28,"Palermo","Motociclismo","Riso","Fallout 3","The Wolf Of Wall Street","L'Ombre del Sole","Briciola")
+let alessandro = ["Alessandro","Castelli", 28 ,"Palermo","Motociclismo","Riso","Fallout 3","The Wolf Of Wall Street","L'Ombra del Sole","Briciola"]
 
