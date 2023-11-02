e77340b8de52bd9c568979c08ed2460c77792423
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2023-11-02 09:54:24 +0100

    aggiornato annagiusi

--- team.js
+++ team.js
@@ -1,14 +1,14 @@
-function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-    this.city=city;
-    this.hobby=hobby;
-    this.favFood=favFood;
-    this.favVideogame=favVideogame;
-    this.favFilm=favFilm;
-    this.favFood=favBook;
-    this.petName=petName;
+let Persona = 
+    annagiusi = {
+    name : "Annagiusi",
+    surname: "Volpe",
+    age: 27,
+    city: "Salerno",
+    hobby: "gym",
+    favFood: "sushi",
+    favVideogame: "Skryrim",
+    favFilm: "Shutter Island",
+    favBook: "Neuromarketing",
+    petName: "Bug",
 }
 
-let annagiusi = new persona("Annagiusi", "Volpe", 27,"Salerno","Gym","sushi","Skyrim","Shutter Island","Neuromarketing","Bug");
