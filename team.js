1d029477b0d68318ab246198a76d3d2cce611d44
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-11-02 09:56:58 +0100

    ChiaRa

--- team.js
+++ team.js
@@ -1,14 +1,13 @@
-function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-    this.city=city;
-    this.hobby=hobby;
-    this.favFood=favFood;
-    this.favVideogame=favVideogame;
-    this.favFilm=favFilm;
-    this.favFood=favBook;
-    this.petName=petName;
-}
-let ChiaraRa = new persona ("Chiara", "Rapisardi", 27, "Palermo", "Natura", "Mandorle", "", "Interstate 60", "L'alchimista", "Ernestina");
-console.log(ChiaraRa);
+
+let Persona =[ 
+    ChiaRa={
+name: "Chiara",
+    surname: "Rapisardi",
+    age: 27,
+    city: "Palermo",
+    hobby:"Natura",
+    favoriteFood:"Mandorle",
+    favoriteVideoGame:"",
+    favoriteFilm:"Interstate 60",
+    favoriteBook:"L'alchimista",
+    petName: "Ernestina",}]
