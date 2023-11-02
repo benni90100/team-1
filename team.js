d1d0387c1c6ff6f399ec1287146e71f2fd34a17f
Enrico Caliandro <enrico.caliandro.94@gmail.com>
2023-11-02 09:57:02 +0100

    modifica al file

--- team.js
+++ team.js
@@ -1,13 +1,2 @@
-function persona (name, surname, age, city, hobby, favFood, favVideogame, favFilm, favBook, petName){
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-    this.city=city;
-    this.hobby=hobby;
-    this.favFood=favFood;
-    this.favVideogame=favVideogame;
-    this.favFilm=favFilm;
-    this.favFood=favBook;
-    this.petName=petName;
-}
+
 
