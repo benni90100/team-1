3f549f6a77a84b54122384d6ccf79f46c70d97ce
Enrico Caliandro <enrico.caliandro.94@gmail.com>
2023-11-02 20:45:00 +0100

    Enrico

--- team.js
+++ team.js
@@ -1,2 +1,14 @@
-
-
+let Persona = [
+    Enrico = {
+        name: "Enrico",
+        surname: "Caliandro",
+        age: 28,
+        city: "Grottaglie",
+        hobby: "Drawing",
+        favoriteFood: "Pasta",
+        favoriteVideoGame: "Crash Bandicoot",
+        favoriteFilm: "Into the Spiderverse",
+        favoriteBook: "Il manuale del guerriero della luce",
+        petName: "Miele",
+    }
+]
