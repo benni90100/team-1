22bbce9d63ba4143f212324d9b0d8719b5e51c01
benni90100 <micelibenedetto800@gmail.com>
2023-11-03 08:59:21 +0100

    bugfix

--- team.js
+++ team.js
@@ -1,19 +1,21 @@
-Persona=[ 
-Chiara = {
-    name: "paluzzi",
-    surname: "chiara",
-    age: 23,
-    city: "civita castellana",
-    hobby: "ballare",
-    favoriteFood: "fettuccine",
-    favoriteVideoGame: "CoD",
-    favoriteFilm: "darty dancing",
-    favoriteBook: "tre volte te",
-    petName: "chloe",
-},
-]
 
-let Persona = [alessandro = {
+
+let Persona = [
+
+    Chiara = {
+        name: "paluzzi",
+        surname: "chiara",
+        age: 23,
+        city: "civita castellana",
+        hobby: "ballare",
+        favoriteFood: "fettuccine",
+        favoriteVideoGame: "CoD",
+        favoriteFilm: "darty dancing",
+        favoriteBook: "tre volte te",
+        petName: "chloe",
+    },
+
+    alessandro = {
     name: "Alessandro",
     surname: "Castelli",
     age: 28,
